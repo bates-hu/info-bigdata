27248ddfd96c8693225d12ece41307e9da61e841
bates-hu <hbc0204@foxmail.com>
2020-10-08 22:16:19 -0700

    update

--- src/main/java/edu/neu/info7255/bates/controller/MainController.java
+++ src/main/java/edu/neu/info7255/bates/controller/MainController.java
@@ -1,38 +1,25 @@
 package edu.neu.info7255.bates.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.neu.info7255.bates.service.RedisService;
 import edu.neu.info7255.bates.utils.Constants;
 import edu.neu.info7255.bates.utils.ExceptionResponse;
 import edu.neu.info7255.bates.utils.IDUtil;
 import edu.neu.info7255.bates.utils.JsonValidator;
-import io.netty.handler.codec.json.JsonObjectDecoder;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 
 
